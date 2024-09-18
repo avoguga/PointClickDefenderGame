7734dcfe3bd1fb239f08a3f433616d4d4209ef0d
Gabriel <gabrielrosendoleitesilva@gmail.com>
2024-09-18 11:28:52 -0300

    refactor: Grid Improvement - Scripts: Improvement [Grid].

--- Assets/Scripts/Testing.cs
+++ Assets/Scripts/Testing.cs
@@ -1,18 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using CodeMonkey.Utils;
 
 public class Testing : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Grid grid;
+    
+    private void Start()
     {
-        
+        grid = new Grid (4, 2, 2f);
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+    
+    private void Update() {
+        if(Input.GetMouseButtonDown(0)) {
+            grid.SetValue(UtilsClass.GetMouseWorldPosition(), 56);
+    }
+    
     }
 }
